e220bd30e8424125aee0d4b0ccc039b6a9c233bd
Marcio26 <31868285+Marcio26@users.noreply.github.com>
2021-11-15 13:28:54 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -3,40 +3,23 @@ package empresa;
 import java.util.Scanner;
 
 public class Principal {
-	
-	static Empregado[] vetor = new Empregado[2];
+
 	static Scanner entrada = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int y = 0;
-		for (int i = 0; i <= 1; i++) { // criando duas instancias do objeto empregado;
-		 vetor[i] = new Empregado();
-		}
+		Empregado emp = new Empregado("", "", 0);
+		Empregado emp2 = new Empregado("", "", 0);
 
-		while(y<2){
-		System.out.println("Informe o nome do empregado: ");
-		vetor[y].nome = entrada.next();
-		// COM NEXTLINE() ESTAVA DUPLICANDO, 
-		//NÃO SENDO POSSIVEL COLOCAR O NOME NA POSIÇÃO 1, PULAVA O NOME[1], 
-		//SÓ CONSEGUIA O SOBRENOME DA POSIÇÃO 1 
-		//NOME[0] E SOBRENOME[0] ESTAVA OK
+		emp.setNome("Joao");
+		emp.setSobrenome("Silva");
+		emp.setSalario(1000);
 
-		System.out.println("Informe o sobrenome empregado: ");
-		vetor[y].sobrenome = entrada.next();
-		//COM NEXTLINE() ESTAVA DUPLICANDO, 
-		//NÃO SENDO POSSIVEL COLOCAR O NOME NA POSIÇÃO 1, PULAVA O NOME[1], 
-		//SÓ CONSEGUIA O SOBRENOME DA POSIÇÃO 1
-		//NOME[0] E SOBRENOME[0] ESTAVA OK
+		emp2.setNome("Paulo");
+		emp2.setSobrenome("Santos");
+		emp2.setSalario(2000);
 
-		System.out.println("Informe o salário anual do empregado: ");
-		vetor[y].salario = entrada.nextDouble();
+		emp.mostrarEmpregado();
+		emp2.mostrarEmpregado();
 
-		y++;
-		}
-		for (int i = 0; i <= 1; i++) {
-			System.out.println("O empregado " + vetor[i].nome + " " + vetor[i].sobrenome + " terá um aumento de "
-					+ vetor[i].aumentarSalario());
-			System.out.println("Salário anual: " + vetor[i].retornarAumento());
-		}
 	}
 }
